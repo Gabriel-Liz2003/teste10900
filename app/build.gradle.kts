5c97b86bca59507d2b8b4a252e0770268344cb55
Gabriel-Liz <102968582+Gabriel-Liz2003@users.noreply.github.com>
2026-08-18 17:36:21 -0300

    Bump GameDrop to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.gabriel.gamedrop"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
